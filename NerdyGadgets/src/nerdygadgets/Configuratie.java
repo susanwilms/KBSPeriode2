a24676c7d697a01dcea87ce488045cbeda6b95cc
swilms8835 <swilms8835@student.landstede.nl>
2019-05-03 11:18:06 +0200

    Configuratie zoals klassendiagram

--- NerdyGadgets/src/nerdygadgets/Configuratie.java
+++ NerdyGadgets/src/nerdygadgets/Configuratie.java
@@ -5,10 +5,32 @@
  */
 package nerdygadgets;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author susanwilms
  */
 public class Configuratie extends Server{
+    // variabelen voor configuratie
+    private ArrayList samenstelling;
+    private double percentageDoei;
     
+    // constructor met super() voor de server class
+    public Configuratie(double percentageDoei){
+        super("", 2, 2.5);
+        this.percentageDoei = percentageDoei;
+    }
+    
+    // functie berekening
+    public int Berekening(){
+        // pseudocode
+        return 2;
+    }
+    
+    // fucntie voor berekenen voor totaalprijs
+    public int BerekenTotaalPrijs(){
+        // pseudocode
+        return 2;
+    }
 }
