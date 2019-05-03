ca017d961edbce817b5cc87740dfd94c7b937053
swilms8835 <swilms8835@student.landstede.nl>
2019-05-03 11:09:56 +0200

    Server.java volgens klassendiagram geprogrammeerd

--- NerdyGadgets/src/nerdygadgets/Server.java
+++ NerdyGadgets/src/nerdygadgets/Server.java
@@ -10,5 +10,42 @@ package nerdygadgets;
  * @author susanwilms
  */
 public class Server {
+    // Variabelen declareren 
+    private String  naam;
+    private int     prijs;
+    private double  beschikbaarheid;
+    private int     beschikbare_tijd;
+    private int     processorbelasting;
+    private int     diskruimte;
+    
+    // constructor om een server mee aan te maken wanneer alleen de naam, prijs en beschikbaarheid beschikbaar is. 
+    public Server (String naam, int prijs, double beschikbaarheid){
+        this.naam               = naam;
+        this.prijs              = prijs;
+        this.beschikbaarheid    = beschikbaarheid;
+        this.beschikbare_tijd   = 0;
+        this.processorbelasting = 0;
+        this.diskruimte         = 0;        
+    }
+    
+    // constructor(overloading) om een server mee aan te maken wanneer alleen de naam, prijs, beschikbaarheid, processorbelasting en diskruimte beschikbaar zijn. 
+    public Server (String naam, int prijs, double beschikbaarheid, int processorbelasting, int diskruimte){
+        this.naam               = naam;
+        this.prijs              = prijs;
+        this.beschikbaarheid    = beschikbaarheid;
+        this.beschikbare_tijd   = 0;
+        this.processorbelasting = processorbelasting;
+        this.diskruimte         = diskruimte; 
+    }
+    
+    // getters
+    public String getNaam() {
+        return naam;
+    }
+
+    public int getPrijs() {
+        return prijs;
+    }
+    
     
 }
