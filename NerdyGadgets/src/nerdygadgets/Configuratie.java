517623b286f87954a17a8967229ae53c0ca9bdba
swilms8835 <swilms8835@student.landstede.nl>
2019-05-06 10:56:27 +0200

    Nerdygadgets package veranderd in configuratie

--- NerdyGadgets/src/nerdygadgets/Configuratie.java
+++ NerdyGadgets/src/nerdygadgets/Configuratie.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package NerdyGadgets;
+package nerdygadgets;
 
 import java.util.ArrayList;
 
