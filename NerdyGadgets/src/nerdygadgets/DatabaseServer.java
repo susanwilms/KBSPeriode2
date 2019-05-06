708459212503a1b0748993af7f1261191387d954
swilms8835 <swilms8835@student.landstede.nl>
2019-05-06 09:15:48 +0200

    classes toegevoegd

---
+++ NerdyGadgets/src/nerdygadgets/DatabaseServer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nerdygadgets;
+
+/**
+ *
+ * @author susanwilms
+ */
+public class DatabaseServer extends Server{
+    
+    
+    public DatabaseServer(){
+        super("", 1, 1);
+    }
+}
---
+++ NerdyGadgets/src/nerdygadgets/PFsense.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nerdygadgets;
+
+/**
+ *
+ * @author susanwilms
+ */
+public class PFsense extends Server{
+    
+    public PFsense(){
+        super(" ", 1, 1);
+    }
+}
