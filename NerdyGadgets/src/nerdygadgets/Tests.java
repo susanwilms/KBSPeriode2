46904b200f155d8ad21595daea82eeafe1c87e4c
joerygrolleman <s1128641@student.windesheim.nl>
2019-05-06 11:02:39 +0200

    Update Tests.java

--- NerdyGadgets/src/nerdygadgets/Tests.java
+++ NerdyGadgets/src/nerdygadgets/Tests.java
@@ -21,9 +21,7 @@ public class Tests {
         Webserver a = new Webserver("HAL9001W", 2200, 0.8);
         Webserver b = new Webserver("HAL9002W", 3200, 0.9);
         Webserver c = new Webserver("HAL9003W", 5100, 0.95);
-        DatabaseServer d = new DatabaseServer("", 5100, 0.9);
-        DatabaseServer e = new DatabaseServer("", 7700, 0.95);
-        DatabaseServer f = new DatabaseServer("", 12200, 0.98);
+        DatabaseServer d = new DatabaseServer("", 2000, 0.90);
         Firewall g = new Firewall("", 2000, 0.9999);
         DBloadbalancer h = new DBloadbalancer("", 2000, 0.9999);
         
