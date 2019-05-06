e8b60c45854426335788f4ab0bc6f1c5c19d70e6
joerygrolleman <s1128641@student.windesheim.nl>
2019-05-06 09:16:06 +0200

    Create Webserver.java

---
+++ NerdyGadgets/src/nerdygadgets/Webserver.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nerdygadgets;
+
+/**
+ *
+ * @author joery
+ */
+public class Webserver extends Server {
+    public Webserver(String naam, int prijs, double beschikbaarheid){
+        super(naam, prijs, beschikbaarheid); 
+    }
+    
+    public Webserver(String naam, int prijs, double beschikbaarheid, int processorbelasting, int diskruimte){
+        super(naam, prijs, beschikbaarheid, processorbelasting, diskruimte); 
+    }
+}
