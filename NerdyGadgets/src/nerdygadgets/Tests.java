a70dbc86c6e4b40c902fdf63d647a3bf332b252e
joerygrolleman <s1128641@student.windesheim.nl>
2019-05-08 09:57:24 +0200

    Update Tests.java

--- NerdyGadgets/src/nerdygadgets/Tests.java
+++ NerdyGadgets/src/nerdygadgets/Tests.java
@@ -20,9 +20,9 @@ public class Tests {
         Webserver a = new Webserver("HAL9001W", 2200, 0.8);
         Webserver b = new Webserver("HAL9002W", 3200, 0.9);
         Webserver c = new Webserver("HAL9003W", 5100, 0.95);
-        DatabaseServer d = new DatabaseServer("", 5100, 0.90);
-        DatabaseServer e = new DatabaseServer("", 7700, 0.95);
-        DatabaseServer f = new DatabaseServer("", 12200, 0.98);
+        DatabaseServer d = new DatabaseServer("HAL9001DB", 5100, 0.90);
+        DatabaseServer e = new DatabaseServer("HAL9002DB", 7700, 0.95);
+        DatabaseServer f = new DatabaseServer("HAL9003DB", 12200, 0.98);
         PFsense g = new PFsense("", 2000, 0.9999);
         DBloadBalancer h = new DBloadBalancer("", 2000, 0.9999);
         Scherm test = new Scherm(a,b,c,d,e,f,g,h);
