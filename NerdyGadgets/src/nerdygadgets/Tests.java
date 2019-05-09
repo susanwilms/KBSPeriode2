b6ff3d7c04f5e0dcf4d6965f46c4473c9b8d00f6
swilms8835 <swilms8835@student.landstede.nl>
2019-05-09 08:53:18 +0200

    namen pfsense en dbloadbalancer toegevoegd

--- NerdyGadgets/src/nerdygadgets/Tests.java
+++ NerdyGadgets/src/nerdygadgets/Tests.java
@@ -29,8 +29,8 @@ public class Tests {
         DatabaseServer f = new DatabaseServer("HAL9003DB", 12200, 0.98);
 
 
-        PFsense g = new PFsense("f", 2000, 0.9999);
-        DBloadBalancer h = new DBloadBalancer("f", 2000, 0.9999);
+        PFsense g = new PFsense("PFsense", 2000, 0.9999);
+        DBloadBalancer h = new DBloadBalancer("DB loadbalancer", 2000, 0.9999);
 
 
         
