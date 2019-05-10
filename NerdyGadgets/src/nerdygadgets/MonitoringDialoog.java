4f9f2554a046e3ef65af8dd5e6f29a67ff238c28
joerygrolleman <s1128641@student.windesheim.nl>
2019-05-10 12:47:06 +0200

    Update MonitoringDialoog.java

--- NerdyGadgets/src/nerdygadgets/MonitoringDialoog.java
+++ NerdyGadgets/src/nerdygadgets/MonitoringDialoog.java
@@ -5,10 +5,45 @@
  */
 //package nerdygadgets;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 /**
  *
- * @author susanwilms
+ * @author mbred
  */
+public class MonitoringDialoog extends JDialog implements ActionListener {
+    private JLabel Naam = new JLabel("Naam project: " + "Projectnaam");
+    private JLabel Resultaat = new JLabel("Opslaan");
+    private JPanel Boven = new JPanel(null);
+    private String opslaannaam;
+    private int opslaanprijs;
+    private double opslaanpercentage;
+    private Database connectie = new Database();
+    private ArrayList<Server> samenstelling;
+    
+    
+    public MonitoringDialoog(JFrame frame) {
+        super(frame, true);
+        this.setSize(500,300);
+        this.setLayout(null);
+        this.setResizable(false);
+        this.getContentPane().setBackground(Color.WHITE);
+
+        Boven.setBounds(0, 0, 500, 40);
+        
+        
+        Boven.setBounds(0,0, 500,28);
+        Resultaat.setBounds(210,5,100,20);
+        Naam.setBounds(10, 50, 500, 20);
 
 
 /*public class MonitoringDialoog extends JDialog implements ActionListener{
@@ -16,8 +51,25 @@
     
     // for (monitoring.getComponenten():items)
     {
+=======
+        
+        Resultaat.setFont(new Font("Helvetica Neue", Font.PLAIN, 16));
+        Naam.setFont(new Font("Helvetica Neue", Font.PLAIN, 16));
+
+
+        
+        Boven.setBackground(new Color(102,255,255));
+
+        
+        Boven.add(Resultaat);
+        add(Resultaat);
+        add(Boven);
+        add(Naam);
+        
+>>>>>>> 24582bca2beea451fd77333c98ede71c9401dcc2
         
     }
-}
 
+<<<<<<< HEAD
 }*/
+
