50b96a8737ff44782fdb5e18edfbe40a57c029a5
MartijnBre <s1129189@student.windesheim.nl>
2019-05-10 12:37:10 +0200

    .

--- NerdyGadgets/src/nerdygadgets/MonitoringDialoog.java
+++ NerdyGadgets/src/nerdygadgets/MonitoringDialoog.java
@@ -9,6 +9,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -17,78 +18,54 @@ import javax.swing.JPanel;
 
 /**
  *
- * @author susanwilms
+ * @author mbred
  */
-
-
-public class MonitoringDialoog extends JDialog implements ActionListener{
-    private JLabel header = new JLabel("Monitoring");
-    private JButton tab1 = new JButton("Webservers");
-    private JButton tab2 = new JButton("Databaseservers");
-    private JButton tab3 = new JButton("Overige");
+public class MonitoringDialoog extends JDialog implements ActionListener {
+    private JLabel Naam = new JLabel("Naam project: " + "Projectnaam");
+    private JLabel Resultaat = new JLabel("Opslaan");
+    private JPanel Boven = new JPanel(null);
+    private String opslaannaam;
+    private int opslaanprijs;
+    private double opslaanpercentage;
+    private Database connectie = new Database();
+    private ArrayList<Server> samenstelling;
+    
     
     public MonitoringDialoog(JFrame frame) {
         super(frame, true);
-        setSize(900,660);
-        setLayout(null);
-        setResizable(false);
+        this.setSize(500,300);
+        this.setLayout(null);
+        this.setResizable(false);
         this.getContentPane().setBackground(Color.WHITE);
-        
-        
-        
-        header.setBounds                        (0, 0, 900, 40);
-        header.setHorizontalAlignment           (JLabel.CENTER);
-        tab1.setBounds                          (0, 50, 150, 40);
-        tab2.setBounds                          (150, 50, 150, 40);
-        tab3.setBounds                          (300, 50, 150, 40);
-        // bounds voor de blokken
-        
-        header.                         setFont(new Font("Helvetica Neue", Font.PLAIN, 16));
-        tab1.                           setFont(new Font("Helvetica Neue", Font.PLAIN, 16));
-        tab2.                           setFont(new Font("Helvetica Neue", Font.PLAIN, 16));
-        tab3.                           setFont(new Font("Helvetica Neue", Font.PLAIN, 16));
-        // fonts voor de tabs
-        
-        header.setOpaque(true);
-        header.setBackground(new Color(102,255,255));
-        tab1.setOpaque(true);
-        tab2.setOpaque(true);
-        tab3.setOpaque(true);
-        // opaque voor de bokken
-        
-        
-        tab1.addActionListener(this);
-        tab2.addActionListener(this);
-        tab3.addActionListener(this);
-        
-        add(header);
-        add(tab1);
-        add(tab2);
-        add(tab3);
-    }
-    
 
-    
-    
+        Boven.setBounds(0, 0, 500, 40);
+        
+        
+        Boven.setBounds(0,0, 500,28);
+        Resultaat.setBounds(210,5,100,20);
+        Naam.setBounds(10, 50, 500, 20);
+
+
+        
+        Resultaat.setFont(new Font("Helvetica Neue", Font.PLAIN, 16));
+        Naam.setFont(new Font("Helvetica Neue", Font.PLAIN, 16));
+
+
+        
+        Boven.setBackground(new Color(102,255,255));
+
+        
+        Boven.add(Resultaat);
+        add(Resultaat);
+        add(Boven);
+        add(Naam);
+        
+        
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == tab1) {
-            tab1.setBackground(new Color(204,255,255));
-            tab2.setBackground(Color.WHITE);
-            tab3.setBackground(Color.WHITE);
-            // add blokken van servers
-        }
-        if(e.getSource() == tab2) {
-            tab1.setBackground(Color.WHITE);
-            tab2.setBackground(new Color(204,255,255));
-            tab3.setBackground(Color.WHITE);
-        }
-        if(e.getSource() == tab3) {
-            tab1.setBackground(Color.WHITE);
-            tab2.setBackground(Color.WHITE);
-            tab3.setBackground(new Color(204,255,255));
-        }
-        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
 }
+
